12b8201b3083511d75faa7c2d18e0d743abc7b3c
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-01-12 14:53:57 -0300

    refactor: move states to the right component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,32 +6,17 @@ import quizCompleteImg from "../assets/quiz-complete.png";
 import Question from "./Question";
 
 export default function Quiz() {
-  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
 
-  const activeQuestionIndex =
-    answerState === "" ? userAnswers.length : userAnswers.length - 1;
+  const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === questions.length;
 
-  const handleSelectAnswer = useCallback(
-    function handleSelectAnswer(selectedAnswer) {
-      setAnswerState("answered");
-      setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
-
-      setTimeout(() => {
-        if (selectedAnswer === questions[activeQuestionIndex].answers[0]) {
-          setAnswerState("correct");
-        } else {
-          setAnswerState("wrong");
-        }
-
-        setTimeout(() => {
-          setAnswerState("");
-        }, 2000);
-      }, 1000);
-    },
-    [activeQuestionIndex]
-  );
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+  },
+  []);
 
   const handleSkipAnswer = useCallback(
     () => handleSelectAnswer(null),
@@ -50,13 +35,10 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <Question
-        questionText={questions[activeQuestionIndex].text}
-        answers={questions[activeQuestionIndex].answers}
-        answerState={answerState}
-        selectedAnswer={userAnswers[userAnswers.length - 1]}
         onSelectAnswer={handleSelectAnswer}
         onSkipAnswer={handleSkipAnswer}
         key={activeQuestionIndex}
+        index={activeQuestionIndex}
       />
     </div>
   );
